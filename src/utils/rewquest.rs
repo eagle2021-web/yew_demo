46ecd09c3d9c2ce0ebd2e3010dda467b38f02bd3
eagle2021-web <chenwangying16@163.com>
2024-04-06 22:55:21 +0800

    reqwest解决问题

--- src/utils/rewquest.rs
+++ src/utils/rewquest.rs
@@ -5,6 +5,9 @@ use yew::services::fetch::Response;
 use yew::{Component, ComponentLink, Html, html, ShouldRender};
 use anyhow::Error;
 use crate::log_to_console;
+use http::Uri;
+use reqwest::{Client};
+use crate::utils::reader_util::ReaderUtil;
 
 pub struct JFrameworkService {
     fetch_service: FetchService,
@@ -29,23 +32,21 @@ impl Component for JFrameworkService {
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg { Msg::FetchResponse(url) => {
-            // log_to_console(&url);
-            let get_request = Request::get("http://localhost:8003/hello")
-                .header("Connection", "keep-alive")
-                .body(Nothing)
-                .expect("Failed to build request.");
             let link = self.link.clone();
-            let callback = link.callback(|response: Response<Result<String, Error>>| {
-                log_to_console(&format!("response = {} ", response.status()));
+            wasm_bindgen_futures::spawn_local(async move {
+                let client = Client::new();
+                let url = "http://localhost:8003/hello"; // 修改为您的实际URL
+                let response = client.get(url).send().await.expect("send err.");
+
                 if response.status().is_success() {
-                    log_to_console("ok");
-                    return Msg::Ok;
+                    let a = response.text().await.unwrap();
+                    log_to_console(&a);
+                    // Ok(response.text().await?)
                 } else {
-                    log_to_console("NOT ok");
-                    return Msg::Ok;
-                }
+                    // 处理非成功响应，可以根据需要进行处理
+                    log_to_console(&format!("Request failed with status: {}", response.status()));
+                };
             });
-            let task = FetchService::fetch(get_request, callback);
         }
             _ => {}
         }
